07bbf923001ed9ebc38f251c5b320ae63b3c6df4
Yulize777 <ruslan602100@gmail.com>
2023-05-17 20:21:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Clock from "./Clock";
+import './style.scss'
+const App = () => {
+    return (
+        <div>
+          <Clock/>
+        </div>
+    );
+};
+
+export default App;
